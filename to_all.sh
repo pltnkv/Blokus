f6c3ffea05594d110f80b57fe6a67e711c4ce0f6
Pltnkv <pltnkv0@gmail.com>
2013-09-06 01:54:42 +0600

    Подготовка файловой структуры

---
+++ to_all.sh
@@ -0,0 +1,4 @@
+scp -r /home/oleg/WebstormProjects/Blokus/app root@88.198.93.139:/home/oleg/server
+scp -r /home/oleg/WebstormProjects/Blokus/client root@88.198.93.139:/home/oleg/server
+scp -r /home/oleg/WebstormProjects/Blokus/node_modules root@88.198.93.139:/home/oleg/server
+
---
+++ to_app_client.sh
@@ -0,0 +1,3 @@
+scp -r /home/oleg/WebstormProjects/Blokus/app root@88.198.93.139:/home/oleg/server
+scp -r /home/oleg/WebstormProjects/Blokus/client root@88.198.93.139:/home/oleg/server
+
